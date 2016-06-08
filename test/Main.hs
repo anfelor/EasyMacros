b175bb12b1084c85df7127e9d1950c2b49e97e6b
Anton Felix Lorenzen <anfelor@web.de>
2016-06-08 15:04:41 +0200

    Removed unused imports, -XOverloadedLists in tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,49 +1,49 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedLists #-}
 
 module Main where
 
 import Data.List
 import Data.List.NonEmpty (NonEmpty(..))
-import Language.Haskell.TH.Macro
 import Language.Haskell.TH.StandardMacros
 
 import System.Directory
 
 test1 :: IO ()
 test1 = $(do_ 
-    ( "dirs <- getDirectoryContents \"/\"" :| 
-    [ "let prefix = '/'"
+    [ "dirs <- getDirectoryContents \"/\"" 
+    , "let prefix = '/'"
     , "let suffix = prefix"
     , "let stdout str = putStrLn str"
     , "mapM_ (stdout . (++[suffix]) . (prefix:)) dirs"
-    ]))
+    ])
 
 test2 :: Int
 test2 = $(cond
-    ( "False -> 1" :|
-    [ "2 == 3 -> 2"
+    [ "False -> 1"
+    , "2 == 3 -> 2"
     , "2 == 2 -> 3" -- try changing it to False
-    ]))
+    ])
 
 test3 :: [Int]
 test3 = $( apply
-    ( "[1..(10*365)]" :|
-    [ "map (replicate 3)"
+    [ "[1..(10*365)]"
+    , "map (replicate 3)"
     , "concat"
     , "take 10"
-    ]))
+    ])
 
 test4 :: String
 test4 = intercalate "\n" $(list
-    ( "\"Hello World!\"" :|
-    [ "\"How are you?\"" 
-    , "\"Yours, Anton\""]))
+    [ "\"Hello World!\""
+    , "\"How are you?\"" 
+    , "\"Yours, Anton\""])
 
 test5 :: Maybe [Int]
-test5 = $(conc 
-    ( "Just [3]" :|
-    [ "Just [4]" 
-    , "Nothing" ]))
+test5 = $(conc
+    [ "Just [3]"
+    , "Just [4]" 
+    , "Nothing" ])
     
 main :: IO ()
 main = do
